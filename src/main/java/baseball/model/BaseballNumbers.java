6e1f6a1cc358a3397c99073c71dc4189b07f209c
0n1dev <all_mind@naver.com>
2022-09-28 21:44:58 +0900

    feat(domain): BaseballNumber & BaseballNumbers 추가
    - BaseballNumber & BaseballNumbers 추가

---
+++ src/main/java/baseball/model/BaseballNumbers.java
@@ -0,0 +1,32 @@
+package baseball.model;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BaseballNumbers {
+
+    private final Set<BaseballNumber> numbers= new LinkedHashSet<>();
+
+    public BaseballNumbers(List<Integer> numbers) {
+        validateSize(numbers);
+
+        for (Integer i : numbers) {
+            BaseballNumber baseballNumber = new BaseballNumber(i);
+            validateDuplicateNumber(baseballNumber);
+            this.numbers.add(baseballNumber);
+        }
+    }
+
+    private void validateSize(final List<Integer> numbers) {
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("야구 게임 숫자 리스트는 3자리 숫자 입니다.");
+        }
+    }
+
+    private void validateDuplicateNumber(final BaseballNumber baseballNumber) {
+        if (numbers.contains(baseballNumber)) {
+            throw new IllegalArgumentException("중복 된 숫자는 들어올 수 없습니다.");
+        }
+    }
+}
